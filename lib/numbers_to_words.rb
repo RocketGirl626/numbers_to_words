ac00e7707dc4e4914eae9093a3bc9162f69a0654
Guest User <student@epicodus.com>
2015-04-21 16:56:35 -0700

    converts strings through 99 to english numbers

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -3,18 +3,26 @@ class Fixnum
     singles = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
     teens = { 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
     tens = { 2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety" }
+    output_number =""
+#    if self>999 && self<10000
+#      output_number = ouput_number.concat(thousands_output)
+#    if self>99 && self<1000
+#      single = self.%(10)
+#      ten = self.-(single)./(10).%(10)
+#      hundred = self.-(ten*10).-(single)./(100)
+#      output_number = output_number.concat(hundreds_output)
     if self == 0
-      output_number = "zero"
-    elsif self>9 && self<20
-      output_number = teens.fetch(self)
+      output_number = ("zero")
     elsif self>19 && self<100
       single = self.%(10)
       ten = self.-(single)./(10)
       output_number = (tens.fetch(ten).concat(" ").concat(singles.fetch(single)))
-      print (output_number)
+    elsif self>9 && self<20
+      output_number = output_number.concat(teens.fetch(self))
     else
       output_number = singles.fetch(self)
     end
+
     output_number
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -14,4 +14,10 @@ describe('Fixnum#numbers_to_words') do
   it("is an english representation of numbers up to 9999") do
    expect((21).numbers_to_words()).to(eq("twenty one"))
  end
+  it ("is an english representation of numbers up to 9999") do
+  expect ((453).numbers_to_words()).to(eq("four hundred fifty three"))
+ end
+ it ("is an english representation of numbers up to 9999") do
+   expect ((5253).numbers_to_words()).to(eq("five thousand two hundred fifty three"))
+ end
 end
